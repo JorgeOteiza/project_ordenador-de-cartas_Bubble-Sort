669b7df794a5705e3f646117edfbe7f02b68d761
Jorge Oteiza <oteiza.jor@gmail.com>
2024-02-17 23:33:43 +0000

    solución styles cards, lógica lista, ajustando detalles

--- src/app.js
+++ src/app.js
@@ -11,13 +11,10 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Función para generar una carta
 function generateCard(randomSuit, randomCardNumber) {
-  // Crea la carta
   const card = document.createElement("div");
   card.className = "card";
 
-  // Crea los elementos para los símbolos en las esquinas
   const topSymbol = document.createElement("div");
   topSymbol.className = "corner-symbol top-left";
   topSymbol.textContent = suitsSymbols[randomSuit];
@@ -26,7 +23,6 @@ function generateCard(randomSuit, randomCardNumber) {
   bottomSymbol.className = "corner-symbol bottom-right";
   bottomSymbol.textContent = suitsSymbols[randomSuit];
 
-  // Crea el elemento para el número o letra centrado
   const centeredText = document.createElement("div");
   centeredText.className = "centered-text";
   const cardNumber =
@@ -35,30 +31,26 @@ function generateCard(randomSuit, randomCardNumber) {
       : ["J", "Q", "K", "A"][randomCardNumber - 9];
   centeredText.textContent = cardNumber;
 
-  // Verifica si la carta es roja y cambia el color del símbolo de corazones a rojo
   if (randomSuit === "heart" || randomSuit === "diamond") {
     topSymbol.style.color = "red";
     bottomSymbol.style.color = "red";
   }
 
-  // Agrega los elementos a la carta
   card.appendChild(topSymbol);
   card.appendChild(centeredText);
   card.appendChild(bottomSymbol);
 
-  return card.outerHTML; // Devuelve el HTML completo de la carta
+  return card.outerHTML;
 }
 
-// Función para generar cartas automáticamente
 function generateCards() {
   const cardsContainer = document.getElementById("cardsContainer");
 
   for (let i = 1; i <= 13; i++) {
     const randomSuitIndex = getRandomNumber(0, 3);
     const randomCardNumber = getRandomNumber(0, 13);
-    const randomSuit = Object.values(suitsSymbols)[randomSuitIndex];
+    const randomSuit = Object.keys(suitsSymbols)[randomSuitIndex];
 
-    // Crea una carta y agrega al contenedor
     cardsContainer.insertAdjacentHTML(
       "beforeend",
       generateCard(randomSuit, randomCardNumber)
@@ -66,7 +58,6 @@ function generateCards() {
   }
 }
 
-// Función para ordenar las cartas con Bubble Sort
 function bubbleSort() {
   const cardsContainer = document.getElementById("cardsContainer");
   const cardsArray = Array.from(cardsContainer.children);
@@ -89,8 +80,5 @@ function bubbleSort() {
     }
   }
 }
-// Llama a la función para generar cartas al cargar la página
-generateCards();
 
-// Llama a la función para ordenar las cartas
-bubbleSort();
+generateCards();
