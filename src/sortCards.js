c57509fe3b43bfcb075cc143140394847652a1cb
JorgeOteiza <oteiza.jor@gmail.com>
2024-02-29 22:46:01 -0300

    problemas, problemas, problemas y más problemas...

--- src/sortCards.js
+++ src/sortCards.js
@@ -4,11 +4,42 @@ export function bubbleSortCards() {
   const cardsContainer = document.getElementById("cardsContainer");
   if (!cardsContainer) {
     // console.error("No se pudo encontrar el contenedor de cartas");
-    return;
+    return Promise.resolve([]);
   }
   const allCards = Array.from(cardsContainer.children);
 
-  return bubbleSort(allCards);
+  return animateBubbleSort(allCards);
+}
+
+function animateBubbleSort(cards) {
+  return new Promise((resolve, reject) => {
+    const bubbleLog = bubbleSort(cards);
+    let i = 0;
+    const intervalId = setInterval(() => {
+      if (i < bubbleLog.length) {
+        const logItem = bubbleLog[i];
+        performSwap(cards, logItem);
+        i++;
+      } else {
+        clearInterval(intervalId);
+        resolve(bubbleLog);
+      }
+    }, 1000);
+  });
+}
+
+function performSwap(cards, logItem) {
+  const [index1, index2] = extractIndices(logItem);
+  const tempStyle = cards[index1].style.transform;
+  cards[index1].style.transform = cards[index2].style.transform;
+  cards[index2].style.transform = tempStyle;
+  cards[index1].classList.toggle("sorted");
+  cards[index2].classList.toggle("sorted");
+}
+
+function extractIndices(logItem) {
+  const indices = logItem.match(/\d+/g);
+  return [parseInt(indices[0]), parseInt(indices[1])];
 }
 
 function customBubbleSort(elementList) {
@@ -25,5 +56,3 @@ function customBubbleSort(elementList) {
 
   return elementList;
 }
-
-export { bubbleSort, customBubbleSort };
